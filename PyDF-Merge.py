b1493f5fd8530a79882232aaded95d22026a45f3
TritoLL <53952040+TritoLL@users.noreply.github.com>
2021-10-18 17:42:53 -0500

    create output directory if it does not exist

--- PyDF-Merge.py
+++ PyDF-Merge.py
@@ -24,12 +24,16 @@ parser.add_argument('input', metavar="pdfToMerge",
 args = parser.parse_args()
 
 # Define output folder name and build output folder path
-output_folder_name = "PyDF-Merge"
-output_folder_path = f"{os.path.dirname(__file__)}\\{output_folder_name}\\"
+output_folder_name = "Merged PDFs"
+output_folder_path = f"{os.path.dirname(__file__)}\\{output_folder_name}"
+
+# Create the output directory
+os.makedirs(output_folder_path, exist_ok=True)
 
 # Define output file name and build output file path
-output_file_name = args.output
-output_file_path = f"{output_folder_path}\\{output_file_name}.pdf"
+output_file_name = args.output if args.output.endswith(
+    ".pdf") else args.output + ".pdf"
+output_file_path = f"{output_folder_path}\\{output_file_name}"
 
 # Create PDF Merger object
 merger = PdfFileMerger()
@@ -38,10 +42,22 @@ merger = PdfFileMerger()
 try:
     for x in args.input:
         merger.append(x)
+
 except FileNotFoundError:
+    # If the input file was not found...
     print(
-        f'ERROR: \"{x}\" could not be found, double-check the file name and try again.')
+        f"ERROR: \"{x}\" could not be found, double-check the file name and try again.")
+    sys.exit()
+
+except OSError as e:
+    # If the input file was not a PDF file...
+    if (e.args[0] == 22):
+        print(f"ERROR: {x} is not a valid PDF file.")
+
+except:
+    print("ERROR: an unknown error has occurred. Please try again.")
     sys.exit()
 
 # Write the finished PDF to a file
-merger.write(output_folder_path)
+merger.write(output_file_path)
+print(f"SUCCESS: {len(args.input)} files were merged to {output_file_path}")
